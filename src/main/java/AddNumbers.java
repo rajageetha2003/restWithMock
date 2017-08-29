93afa623ca767d9194c90c56c5227f149f5b7b7d
unknown <geetha.raja@clearpoint.co.nz>
2017-08-30 03:15:31 +1200

    added missing rest components

--- src/main/java/AddNumbers.java
+++ src/main/java/AddNumbers.java
@@ -1,10 +1,18 @@
-//package main.java;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
 
+@Path("/add/{a}/{b}")
 public class AddNumbers{
 
     public void AddNumbers() {
     }
 
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
     public double addTwoNumbers(double a, double b) {
         return (a + b);
     }
